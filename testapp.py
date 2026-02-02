ccddad2b26b126688afdd47663915ddfd50e9413
NiralDino <131156091+NiralDino@users.noreply.github.com>
2026-02-02 13:09:47 +0000

    Add test for Jenkins

---
+++ testapp.py
@@ -0,0 +1,19 @@
+#This is just a test program for Jenkins
+from app import app
+
+def test_calculation_success():
+    client = app.test_client()
+
+    response = client.post(
+        "/calculation",
+        json={"numbers": [1, 2, 3, 4, 5]}
+    )
+
+    assert response.status_code == 200
+
+    #If the results for the list of numbers isn't shown as below, the test would fail
+    data = response.get_json()
+    assert data["Mean"] == 3
+    assert data["Median"] == 3
+    assert data["Minimum"] == 1
+    assert data["Maximum"] == 5
