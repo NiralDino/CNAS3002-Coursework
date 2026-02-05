83ad195b704002bc110c11285da55e534a98aabe
NiralDino <131156091+NiralDino@users.noreply.github.com>
2026-02-05 12:50:20 +0000

    Refactor unit tests for calculation endpoint
    Refactored unit tests for the Flask app's calculation endpoint, including setup method and validation for valid and invalid input.

--- test_app.py
+++ test_app.py
@@ -6,19 +6,47 @@ sys.path.insert(0, os.path.abspath("."))
 #This is just a test program for Jenkins
 from flaskcoursework_app import app
 
-def test_calculation_success():
-    client = app.test_client()
+#Unit test is implemented to test the Flask app
+import unittest
 
-    response = client.post(
-        "/calculation",
-        json={"numbers": [1, 2, 3, 4, 5]}
-    )
+class TestCalculationEndpoint(unittest.TestCase):
+    def setUp(self):
+        """
+        This method will run before each test.
+        It creates a FLask test client that sends HTTP requests to the application without starting a server.
+        """
+        self.client = app.test_client()
+        self.client.testing = True
 
-    assert response.status_code == 200
+    def test_calculation_success(self):
+        """
+        This test checks that the /calculation endpoint compiles valid numeric input and returns the expected results
+        """
+        response = self.client.post(
+            "/calculation",
+            json={"numbers": [1, 2, 3, 4, 5]}
+        )
+        
+        self.assertEqual(response.status_code, 200)
 
-    #If the results for the list of numbers isn't shown as below, the test would fail
-    data = response.get_json()
-    assert data["Mean"] == 3
-    assert data["Median"] == 3
-    assert data["Minimum"] == 1
-    assert data["Maximum"] == 5
+        #If the results for the list of numbers isn't shown as below, the test would fail
+        data = response.get_json()
+        self.assertEqual(data["Mean"], 5)
+        self.assertEqual(data["Median"], 3)
+        self.assertEqual(data["Minimum"], 1)
+        self.assertEqual(data["Maximum"], 5)
+        
+    def test_calculation_invalid_input(self):
+        """
+        This will check for invalid input
+        """
+        response = self.client.post(
+            "/calculation",
+            json={"numbers": [1, "a", 3]}
+        )
+
+        self.assertEqual(response.status_code, 400)
+        
+#This allows the test file to be run from Jenkins
+if __name__ == "__main__":
+    unittest.main()
